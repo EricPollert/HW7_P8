18258a6a42e8cd7b729b2fd8ad1609e7e38a5912
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-12 03:58:56 +0000

    Added further for loops and comments as well as if statement to check C

--- main.cpp
+++ main.cpp
@@ -9,10 +9,26 @@ int main() {
   cin >> N;
   cin >> C;
   int sort[N];
+  int numbers[C]; // tracks frequency of a specific number in sort[]
+
+  // for loops
   for(int i = 0; i < N; i++)
     {
       cin >> sort[i];
+
   
+  }
+  for(int i = 0; i < N; i++)
+  {
+    for(int i = 0; i < C; i++)
+  {
+    if (sort[N] == C){ // checks if number in sort[] equals C
+      numbers[C - 1]++;
+
+      }
+    
+    }
+
   }
 
 }
