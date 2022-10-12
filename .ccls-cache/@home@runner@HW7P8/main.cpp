6bfe786655104e64ec3e4cd7f5531642b1ebca87
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-12 03:48:11 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW7P8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  int N, C;
+  cin >> N;
+  cin >> C;
+  int sort[N];
+  for(int i = 0; i < N; i++)
+    {
+      cin >> sort[i];
+  
+  }
+
+}
