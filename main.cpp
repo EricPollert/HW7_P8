637612223e83d0892b8d9627c0bf0cccba135312
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-12 04:43:40 +0000

    replicated first nested for loop, corrected which variables are used and where

--- main.cpp
+++ main.cpp
@@ -5,30 +5,58 @@ using namespace std;
 
 int main() {
 
-  int N, C;
+  int N, C, temp;
   cin >> N;
   cin >> C;
   int sort[N];
   int numbers[C]; // tracks frequency of a specific number in sort[]
 
   // for loops
-  for(int i = 0; i < N; i++)
-    {
+  for(int i = 0; i < N; i++){
+    
       cin >> sort[i];
-
+  
   
   }
-  for(int i = 0; i < N; i++)
-  {
-    for(int i = 0; i < C; i++)
-  {
-    if (sort[N] == C){ // checks if number in sort[] equals C
-      numbers[C - 1]++;
+  for(int i = 0; i < N; i++){
+    
+    for(int j = 0; j < C; j++){
+      
+    if (sort[i] == (j + 1)){ // checks if number in sort[] equals C
+      numbers[j]++;
 
       }
-    
     }
 
   }
 
+  for(int i = 0; i < N - 1; i++){
+    
+    for(int j = 0; j < C; j++){
+      
+    if (numbers[sort[i]-1] < numbers[sort[i+1] - 1]){ // checking frequency of N in sort
+      temp = sort[i];
+      sort[i] = sort[i + 1];
+      sort[i + 1] = temp;
+      }
+    else
+
+      if(sort[i] < sort[i + 1]){
+      
+      temp = sort[i];
+      sort[i] = sort[i + 1];
+      sort[i + 1] = temp;
+        
+      }
+
+      
+    }
+      
+  }
+  for(int i = 0; i < N; i++){
+    
+      cout << sort[i];
+  
+  
+  }
 }
